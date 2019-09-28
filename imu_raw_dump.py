63256d94fb6bb7ccabc9529d21b2d094d80e9d77
Neil Balch <neilbalch@gmail.com>
2019-09-27 17:14:37 -0700

    Add csv logger to imu_raw_dump.py

--- imu_raw_dump.py
+++ imu_raw_dump.py
@@ -1,14 +1,34 @@
 import time
+import csv
+import os
 from imu_utils import IMU
-DEL = ". "
+DEL = ", "  # Data item delimiter
+
+file_suffix = 0
+while os.path.isfile("imu_dump" + str(file_suffix) + ".csv"):
+    file_suffix += 1
+
+file = csv.writer(open("imu_dump" + str(file_suffix) + ".csv", 'w'),
+                  delimiter=DEL, quotechar='|', quoting=csv.QUOTE_MINIMAL)
 
 IMU.detectIMU()     # Detect if BerryIMUv1 or BerryIMUv2 is connected.
 IMU.initIMU()       # Initialise the accelerometer, gyroscope and compass
 
-while True:
-    print(str(IMU.readACCx()) + ", " + str(IMU.readACCy()) + ", "
-          + str(IMU.readACCz()) + "\t" + str(IMU.readGYRx()) + ", "
-          + str(IMU.readGYRy()) + ", " + str(IMU.readGYRz()) + "\t"
-          + str(IMU.readMAGx()) + ", " + str(IMU.readMAGy()) + ", "
-          + str(IMU.readMAGz()) + "\t")
-    time.sleep(0.1)
+try:
+    while True:
+        print(str(IMU.readACCx()) + DEL + str(IMU.readACCy()) + DEL
+              + str(IMU.readACCz()) + "\t" + str(IMU.readGYRx()) + DEL
+              + str(IMU.readGYRy()) + DEL + str(IMU.readGYRz()) + "\t"
+              + str(IMU.readMAGx()) + DEL + str(IMU.readMAGy()) + DEL
+              + str(IMU.readMAGz()) + "\t")
+
+        csvLine = (str(IMU.readACCx()) + DEL + str(IMU.readACCy()) + DEL
+                   + str(IMU.readACCz()) + DEL + str(IMU.readGYRx()) + DEL
+                   + str(IMU.readGYRy()) + DEL + str(IMU.readGYRz()) + DEL
+                   + str(IMU.readMAGx()) + DEL + str(IMU.readMAGy()) + DEL
+                   + str(IMU.readMAGz()))
+        file.writerow(csvLine)
+        time.sleep(0.1)
+
+except KeyboardInterrupt:
+    pass
