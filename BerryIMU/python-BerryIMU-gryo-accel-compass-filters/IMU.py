08b2967d35a8d0bdc0550bfa2e188b01fadc9951
Neil Balch <neilbalch@gmail.com>
2019-10-03 19:17:28 -0700

    oop, one more bit

--- BerryIMU/python-BerryIMU-gryo-accel-compass-filters/IMU.py
+++ BerryIMU/python-BerryIMU-gryo-accel-compass-filters/IMU.py
@@ -4,15 +4,19 @@ from LSM9DS0 import *
 from LSM9DS1 import *
 import time
 
+
+
 LSM9DS0 = 1
 
+
+
 def detectIMU():
-    #Detect which version of BerryIMU is connected.
+    #Detect which version of BerryIMU is connected.   
     #BerryIMUv1 uses the LSM9DS0
     #BerryIMUv2 uses the LSM9DS1
     global LSM9DS0
-
-
+    
+    
     try:
         #Check for LSM9DS0
         #If no LSM9DS0 is conencted, there will be an I2C bus error and the program will exit.
@@ -20,10 +24,10 @@ def detectIMU():
         LSM9DS0_WHO_G_response = (bus.read_byte_data(LSM9DS0_GYR_ADDRESS, LSM9DS0_WHO_AM_I_G))
         LSM9DS0_WHO_XM_response = (bus.read_byte_data(LSM9DS0_ACC_ADDRESS, LSM9DS0_WHO_AM_I_XM))
     except IOError as e:
-        print ''        #need to do something here, so we just print a space
+        print('')        #need to do something here, so we just print a space
     else:
         if (LSM9DS0_WHO_G_response == 0xd4) and (LSM9DS0_WHO_XM_response == 0x49):
-            print "Found LSM9DS0"
+            print("Found LSM9DS0")
             LSM9DS0 = 1
 
 
@@ -35,13 +39,18 @@ def detectIMU():
         LSM9DS1_WHO_M_response = (bus.read_byte_data(LSM9DS1_MAG_ADDRESS, LSM9DS1_WHO_AM_I_M))
 
     except IOError as f:
-        print ''        #need to do something here, so we just print a space
+        print('')        #need to do something here, so we just print a space
     else:
         if (LSM9DS1_WHO_XG_response == 0x68) and (LSM9DS1_WHO_M_response == 0x3d):
-            print "Found LSM9DS1"
+            print("Found LSM9DS1")
             LSM9DS0 = 0
 
     time.sleep(1)
+
+
+
+    
+
 
 def writeAG(register,value):
         bus.write_byte_data(ACC_ADDRESS , register, value)
@@ -59,34 +68,38 @@ def writeGRY(register,value):
         bus.write_byte_data(GYR_ADDRESS, register, value)
         return -1
 
-# Accelerometer
+
+
 def readACCx():
         acc_l = bus.read_byte_data(ACC_ADDRESS, OUT_X_L_XL)
         acc_h = bus.read_byte_data(ACC_ADDRESS, OUT_X_H_XL)
-	acc_combined = (acc_l | acc_h <<8)
+        acc_combined = (acc_l | acc_h <<8)
 
-	return acc_combined  if acc_combined < 32768 else acc_combined - 65536
+        return acc_combined  if acc_combined < 32768 else acc_combined - 65536
+
 
 def readACCy():
         acc_l = bus.read_byte_data(ACC_ADDRESS, OUT_Y_L_XL)
         acc_h = bus.read_byte_data(ACC_ADDRESS, OUT_Y_H_XL)
-	acc_combined = (acc_l | acc_h <<8)
+        acc_combined = (acc_l | acc_h <<8)
 
-	return acc_combined  if acc_combined < 32768 else acc_combined - 65536
+        return acc_combined  if acc_combined < 32768 else acc_combined - 65536
+
 
 def readACCz():
         acc_l = bus.read_byte_data(ACC_ADDRESS, OUT_Z_L_XL)
         acc_h = bus.read_byte_data(ACC_ADDRESS, OUT_Z_H_XL)
-	acc_combined = (acc_l | acc_h <<8)
+        acc_combined = (acc_l | acc_h <<8)
 
-	return acc_combined  if acc_combined < 32768 else acc_combined - 65536
+        return acc_combined  if acc_combined < 32768 else acc_combined - 65536
 
-# Magenetometer
+
 def readMAGx():
-		mag_l = bus.read_byte_data(MAG_ADDRESS, OUT_X_L_M)
-		mag_h = bus.read_byte_data(MAG_ADDRESS, OUT_X_H_M)
-		mag_combined = (mag_l | mag_h <<8)
-		return mag_combined  if mag_combined < 32768 else mag_combined - 65536
+        mag_l = bus.read_byte_data(MAG_ADDRESS, OUT_X_L_M)
+        mag_h = bus.read_byte_data(MAG_ADDRESS, OUT_X_H_M)
+        mag_combined = (mag_l | mag_h <<8)
+        return mag_combined  if mag_combined < 32768 else mag_combined - 65536
+
 
 def readMAGy():
         mag_l = bus.read_byte_data(MAG_ADDRESS, OUT_Y_L_M)
@@ -95,6 +108,7 @@ def readMAGy():
 
         return mag_combined  if mag_combined < 32768 else mag_combined - 65536
 
+
 def readMAGz():
         mag_l = bus.read_byte_data(MAG_ADDRESS, OUT_Z_L_M)
         mag_h = bus.read_byte_data(MAG_ADDRESS, OUT_Z_H_M)
@@ -102,13 +116,14 @@ def readMAGz():
 
         return mag_combined  if mag_combined < 32768 else mag_combined - 65536
 
-# Gyroscope
+
 def readGYRx():
         gyr_l = bus.read_byte_data(GYR_ADDRESS, OUT_X_L_G)
         gyr_h = bus.read_byte_data(GYR_ADDRESS, OUT_X_H_G)
         gyr_combined = (gyr_l | gyr_h <<8)
 
         return gyr_combined  if gyr_combined < 32768 else gyr_combined - 65536
+  
 
 def readGYRy():
         gyr_l = bus.read_byte_data(GYR_ADDRESS, OUT_Y_L_G)
@@ -124,26 +139,37 @@ def readGYRz():
 
         return gyr_combined  if gyr_combined < 32768 else gyr_combined - 65536
 
+
+
+
+
+
+
+
+
 def writeACC(register,value):
     if(LSM9DS0):
         bus.write_byte_data(LSM9DS0_ACC_ADDRESS , register, value)
     else:
         bus.write_byte_data(LSM9DS1_ACC_ADDRESS , register, value)
     return -1
+    
 
 def writeMAG(register,value):
     if(LSM9DS0):
         bus.write_byte_data(LSM9DS0_MAG_ADDRESS, register, value)
     else:
-        bus.write_byte_data(LSM9DS1_MAG_ADDRESS , register, value)
+        bus.write_byte_data(LSM9DS1_MAG_ADDRESS , register, value)        
         return -1
 
 def writeGRY(register,value):
     if(LSM9DS0):
         bus.write_byte_data(LSM9DS0_GYR_ADDRESS, register, value)
     else:
-        bus.write_byte_data(LSM9DS1_GYR_ADDRESS , register, value)
+        bus.write_byte_data(LSM9DS1_GYR_ADDRESS , register, value)        
         return -1
+
+
 
 def readACCx():
     if (LSM9DS0):
@@ -156,16 +182,18 @@ def readACCx():
     acc_combined = (acc_l | acc_h <<8)
     return acc_combined  if acc_combined < 32768 else acc_combined - 65536
 
+
 def readACCy():
     if (LSM9DS0):
         acc_l = bus.read_byte_data(LSM9DS0_ACC_ADDRESS, LSM9DS0_OUT_Y_L_A)
         acc_h = bus.read_byte_data(LSM9DS0_ACC_ADDRESS, LSM9DS0_OUT_Y_H_A)
     else:
         acc_l = bus.read_byte_data(LSM9DS1_ACC_ADDRESS, LSM9DS1_OUT_Y_L_XL)
-        acc_h = bus.read_byte_data(LSM9DS1_ACC_ADDRESS, LSM9DS1_OUT_Y_H_XL)
-
+        acc_h = bus.read_byte_data(LSM9DS1_ACC_ADDRESS, LSM9DS1_OUT_Y_H_XL)       
+    
     acc_combined = (acc_l | acc_h <<8)
     return acc_combined  if acc_combined < 32768 else acc_combined - 65536
+
 
 def readACCz():
     if (LSM9DS0):
@@ -174,9 +202,10 @@ def readACCz():
     else:
         acc_l = bus.read_byte_data(LSM9DS1_ACC_ADDRESS, LSM9DS1_OUT_Z_L_XL)
         acc_h = bus.read_byte_data(LSM9DS1_ACC_ADDRESS, LSM9DS1_OUT_Z_H_XL)
-
+    
     acc_combined = (acc_l | acc_h <<8)
     return acc_combined  if acc_combined < 32768 else acc_combined - 65536
+
 
 def readMAGx():
     if (LSM9DS0):
@@ -189,6 +218,7 @@ def readMAGx():
     mag_combined = (mag_l | mag_h <<8)
     return mag_combined  if mag_combined < 32768 else mag_combined - 65536
 
+
 def readMAGy():
     if (LSM9DS0):
         mag_l = bus.read_byte_data(LSM9DS0_MAG_ADDRESS, LSM9DS0_OUT_Y_L_M)
@@ -200,16 +230,19 @@ def readMAGy():
     mag_combined = (mag_l | mag_h <<8)
     return mag_combined  if mag_combined < 32768 else mag_combined - 65536
 
+
 def readMAGz():
     if (LSM9DS0):
         mag_l = bus.read_byte_data(LSM9DS0_MAG_ADDRESS, LSM9DS0_OUT_Z_L_M)
         mag_h = bus.read_byte_data(LSM9DS0_MAG_ADDRESS, LSM9DS0_OUT_Z_H_M)
     else:
         mag_l = bus.read_byte_data(LSM9DS1_MAG_ADDRESS, LSM9DS1_OUT_Z_L_M)
-        mag_h = bus.read_byte_data(LSM9DS1_MAG_ADDRESS, LSM9DS1_OUT_Z_H_M)
+        mag_h = bus.read_byte_data(LSM9DS1_MAG_ADDRESS, LSM9DS1_OUT_Z_H_M)           
 
     mag_combined = (mag_l | mag_h <<8)
     return mag_combined  if mag_combined < 32768 else mag_combined - 65536
+
+
 
 def readGYRx():
     if (LSM9DS0):
@@ -218,9 +251,10 @@ def readGYRx():
     else:
         gyr_l = bus.read_byte_data(LSM9DS1_GYR_ADDRESS, LSM9DS1_OUT_X_L_G)
         gyr_h = bus.read_byte_data(LSM9DS1_GYR_ADDRESS, LSM9DS1_OUT_X_H_G)
-
+    
     gyr_combined = (gyr_l | gyr_h <<8)
     return gyr_combined  if gyr_combined < 32768 else gyr_combined - 65536
+  
 
 def readGYRy():
     if (LSM9DS0):
@@ -244,13 +278,16 @@ def readGYRz():
     gyr_combined = (gyr_l | gyr_h <<8)
     return gyr_combined  if gyr_combined < 32768 else gyr_combined - 65536
 
+
+
+
 def initIMU():
 
     if (LSM9DS0):   #For BerryIMUv1
-
+        
         #initialise the accelerometer
         writeACC(LSM9DS0_CTRL_REG1_XM, 0b01100111)  #z,y,x axis enabled, continuos update,  100Hz data rate
-        writeACC(LSM9DS0_CTRL_REG2_XM, 0b00100000)  #+/- 16G full scale
+        writeACC(LSM9DS0_CTRL_REG2_XM, 0b00011000)  #+/- 8G full scale
 
         #initialise the magnetometer
         writeMAG(LSM9DS0_CTRL_REG5_XM, 0b11110000)  #Temp enable, M data rate = 50Hz
@@ -260,32 +297,20 @@ def initIMU():
         #initialise the gyroscope
         writeGRY(LSM9DS0_CTRL_REG1_G, 0b00001111)   #Normal power mode, all axes enabled
         writeGRY(LSM9DS0_CTRL_REG4_G, 0b00110000)   #Continuos update, 2000 dps full scale
-
+    
     else:       #For BerryIMUv2
         #initialise the gyroscope
         writeGRY(LSM9DS1_CTRL_REG4,0b00111000)      #z, y, x axis enabled for gyro
         writeGRY(LSM9DS1_CTRL_REG1_G,0b10111000)    #Gyro ODR = 476Hz, 2000 dps
-        writeGRY(LSM9DS1_ORIENT_CFG_G,0b00111000)   #Swap orientation
+        writeGRY(LSM9DS1_ORIENT_CFG_G,0b10111000)   #Swap orientation 
 
         #initialise the accelerometer
         writeACC(LSM9DS1_CTRL_REG5_XL,0b00111000)   #z, y, x axis enabled for accelerometer
-        writeACC(LSM9DS1_CTRL_REG6_XL,0b00101000)   #+/- 16g
+        writeACC(LSM9DS1_CTRL_REG6_XL,0b00111000)   #+/- 8g
 
         #initialise the magnetometer
         writeMAG(LSM9DS1_CTRL_REG1_M, 0b10011100)   #Temp compensation enabled,Low power mode mode,80Hz ODR
         writeMAG(LSM9DS1_CTRL_REG2_M, 0b01000000)   #+/-12gauss
         writeMAG(LSM9DS1_CTRL_REG3_M, 0b00000000)   #continuos update
         writeMAG(LSM9DS1_CTRL_REG4_M, 0b00000000)   #lower power mode for Z axis
-
-
-
-
-
-
-
-
-
-
-
-
 
